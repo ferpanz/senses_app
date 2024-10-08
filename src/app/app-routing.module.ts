d21e5d46da2b38bda0c5203ee312e2078b34436a
ferpanz <ferpanz@gmail.com>
2024-10-08 15:47:18 -0300

    Routes listas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConfirmComponent } from './pages/confirm/confirm.component';
+import { FormComponent } from './pages/form/form.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TiposComponent } from './pages/tipos/tipos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path:"confirm",
+  component: ConfirmComponent
+},
+{
+  path:"form",
+  component: FormComponent
+},
+{
+  path:"home",
+  component: HomeComponent
+},
+{
+  path:"tipos",
+  component: TiposComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-import { TiposComponent } from './pages/tipos/tipos.component';
-import { FormComponent } from './pages/form/form.component';
-import { ConfirmComponent } from './pages/confirm/confirm.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    TiposComponent,
-    FormComponent,
-    ConfirmComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
