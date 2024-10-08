a8789d94765c4f7c0526ac25359a17c0253692c9
ferpanz <ferpanz@gmail.com>
2024-10-08 15:10:04 -0300

    pages creadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TiposComponent } from './pages/tipos/tipos.component';
+import { FormComponent } from './pages/form/form.component';
+import { ConfirmComponent } from './pages/confirm/confirm.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TiposComponent,
+    FormComponent,
+    ConfirmComponent
   ],
   imports: [
     BrowserModule,
