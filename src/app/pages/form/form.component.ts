a1ded50ce20c5b5ba570ab5cc65d23af44211cdf
ferpanz <ferpanz@gmail.com>
2024-10-10 16:14:44 -0300

    Home, tipos y form listos con botones funcionando

--- src/app/pages/form/form.component.ts
+++ src/app/pages/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrl: './form.component.css'
 })
 export class FormComponent {
+  constructor(private router: Router){
+
+  }
+  navegar(direccion:string){
+    this.router.navigate([direccion])
+  }
 
 }
--- src/app/pages/tipos/tipos.component.ts
+++ src/app/pages/tipos/tipos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tipos',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './tipos.component.css'
 })
 export class TiposComponent {
+  constructor(private router: Router){
 
+  }
+  navegar(direccion:string){
+    this.router.navigate([direccion])
+  }
 }
