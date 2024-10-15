cab43d17cb201c2379c47d26dce908f9af99993e
ferpanz <ferpanz@gmail.com>
2024-10-15 15:45:04 -0300

    Beta 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,11 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ConfirmComponent } from './pages/confirm/confirm.component';
 import { FormComponent } from './pages/form/form.component';
 import { HomeComponent } from './pages/home/home.component';
 import { TiposComponent } from './pages/tipos/tipos.component';
 
 const routes: Routes = [
-{
-  path:"confirm",
-  component: ConfirmComponent
-},
+
 {
   path:"form",
   component: FormComponent
